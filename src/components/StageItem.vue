b94d52041b035bc982c3337fdcab2754b7b2fd0c
Lalo <work@lalo.im>
2024-12-01 16:34:08 +0800

    Implement UX map generation feature in MyHeader component and enhance StageItem layout
    - Added WebSocket connection for generating UX maps in MyHeader.vue.
    - Implemented loading state management during map generation.
    - Updated StageItem.vue to improve layout with adjusted dimensions for stage display elements.

--- src/components/StageItem.vue
+++ src/components/StageItem.vue
@@ -18,19 +18,19 @@ function getArrayString(arr: string[]) {
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 p-4 max-w-60">
+  <div class="flex flex-col gap-4 p-4 max-w-80">
     <div class="bg-secondary text-secondary-content p-2 rounded-md shadow h-10 overflow-hidden">
       <div class="text-ellipsis overflow-hidden whitespace-nowrap">
         {{ stage.stage_name }}
       </div>
     </div>
-    <div class="bg-base-100 p-2 rounded-md shadow h-20 overflow-hidden text-ellipsis">
+    <div class="bg-base-100 p-2 rounded-md shadow h-32 overflow-hidden text-ellipsis">
       {{ stage.user_behavior }}
     </div>
     <div class="bg-base-100 p-2 rounded-md shadow h-20 overflow-hidden text-ellipsis">
       {{ getArrayString(stage.touchpoints) }}
     </div>
-    <div class="bg-base-100 p-2 rounded-md shadow h-20 overflow-hidden text-ellipsis">
+    <div class="bg-base-100 p-2 rounded-md shadow h-10 overflow-hidden text-ellipsis">
       {{ stage.emotion }}
     </div>
     <div class="bg-base-100 p-2 rounded-md shadow h-28 overflow-hidden text-ellipsis">
